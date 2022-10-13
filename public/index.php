35bb045cf2070c74f9be1d1f30b41d8f8fe6bdff
sheaqx <111645897+sheaqx@users.noreply.github.com>
2022-10-13 10:53:49 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
 
-$name = 'Wilder';
-
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', ['products' => $products]);
